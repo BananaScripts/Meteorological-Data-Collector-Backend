7c88f2c1bcaabc67bb66b1cf5d35f6f2be9a542c
Douglas F. Medeiros <80690245+DouglasMedeiros1@users.noreply.github.com>
2024-10-17 09:21:01 -0300

    Fix: Correcao do build do backend

--- src/Collector/server.ts
+++ src/Collector/server.ts
@@ -1,6 +1,6 @@
 import app from "./source/app";
 
-const port = process.env.PORT || 3080;
+const port = process.env.PORT || 30015;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
