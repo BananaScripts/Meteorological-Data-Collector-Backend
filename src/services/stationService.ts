01cfe4216e15de57fada5b2d24b7748299da1eb6
Bruno Fernandes Campos <brunofernandescampos@gmail.com>
2024-10-20 23:17:43 -0300

    fix: Pequeno ajuste no sistema de estacao

--- src/services/stationService.ts
+++ src/services/stationService.ts
@@ -13,18 +13,26 @@ export const buscarEstacao = async (cod_estacao: number): Promise<Estacao | null
 }
 
 
+<<<<<<< Updated upstream:src/services/stationService.ts
 export const cadastrarEstacao = async(macAdress: string, nome: string, cidade: string, estado: string, numero: number, cep: string): Promise<Estacao> =>{
+=======
+export const cadastrarEstacao = async(macAdress: string, nome: string, cidade: string, estado: string, numero: string, cep: number): Promise<Estacao> => {
+>>>>>>> Stashed changes:src/Controller/services/stationService.ts
     return prisma.estacao.create({
-        data: {macAdress, nome, cidade, estado, numero, cep},
-    })
-}
+        data: {macAdress, nome, cidade, estado, numero: Number(numero), cep: cep.toString()},
+    });
+};
 
+<<<<<<< Updated upstream:src/services/stationService.ts
 export const atualizarEstacao = async(cod_estacao:number, macAdress: string, nome: string, cidade: string, estado: string, numero: number, cep: string): Promise<Estacao> =>{
+=======
+export const atualizarEstacao = async(cod_estacao: number, macAdress: string, nome: string, cidade: string, estado: string, numero: string, cep: number): Promise<Estacao> => {
+>>>>>>> Stashed changes:src/Controller/services/stationService.ts
     return prisma.estacao.update({
         where: {cod_estacao},
-        data: {macAdress, nome, cidade, estado, numero, cep},
-    })
-}
+        data: {macAdress, nome, cidade, estado, numero: Number(numero), cep: cep.toString()},
+    });
+};
 
 export const deletarEstacao = async(cod_estacao: number): Promise<Estacao>=>{
     return prisma.estacao.delete({
